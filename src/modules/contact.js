15595c12f44f1f0274c9f4612ba80b210dab461e
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-06 22:54:30 +0530

    add contact tab contents

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,11 +1,41 @@
+import image from '../assets/contact.png';
+
+const data = [
+    {
+        name: 'John',
+        positon: 'Owner',
+        phone: '+91 52671 78211',
+    },
+    {
+        name: 'Rupert',
+        positon: 'Manger',
+        phone: '+91 78122 87321',
+    },
+    {
+        name: 'Loius',
+        positon: 'Waiter',
+        phone: '+91 67483 12213',
+    },
+    {
+        name: 'Meggan',
+        positon: 'Chef',
+        phone: '+91 23133 32472',
+    },
+];
+
 export default function contact() {
-    const header = document.querySelector('.tabHeader');
-    const description = document.querySelector('.tabDescription');
-    header.textContent = 'Contact';
-    description.textContent =
-        'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\
-        Tenetur distinctio rem cum animi, minima, \
-        facilis delectus earum sint inventore totam dolor autem. \
-        Asperiores nemo porro quibusdam totam inventore! \
-        Libero, consectetur?';
+    const header = document.querySelector('.tab-header');
+    const description = document.querySelector('.tab-description');
+
+    header.textContent = 'Contact Us';
+    description.innerHTML = '';
+    data.forEach((item) => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML += `<p class="name">${item.name}</p>`;
+        card.innerHTML += `<p class="positon">${item.positon}</p>`;
+        card.innerHTML += `<p class="phone">${item.phone}</p>`;
+        card.innerHTML += `<img src=${image} alt="contact">`;
+        description.appendChild(card);
+    });
 }
