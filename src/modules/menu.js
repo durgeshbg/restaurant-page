8aadfc927ceb456e25795c601bda1a6ea14ddd95
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-06 18:40:55 +0530

    add menu module

---
+++ src/modules/menu.js
@@ -0,0 +1,11 @@
+export default function menu() {
+    const header = document.querySelector('.tabHeader');
+    const description = document.querySelector('.tabDescription');
+    header.textContent = 'Menu';
+    description.textContent =
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\
+        Tenetur distinctio rem cum animi, minima, \
+        facilis delectus earum sint inventore totam dolor autem. \
+        Asperiores nemo porro quibusdam totam inventore! \
+        Libero, consectetur?';
+}
