a5e77b6a516491c5146cde6b3234e866eb10920f
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-06 18:38:27 +0530

    create contact module

---
+++ src/modules/contact.js
@@ -0,0 +1,11 @@
+export default function contact() {
+    const header = document.querySelector('.tabHeader');
+    const description = document.querySelector('.tabDescription');
+    header.textContent = 'Contact';
+    description.textContent =
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit.\
+        Tenetur distinctio rem cum animi, minima, \
+        facilis delectus earum sint inventore totam dolor autem. \
+        Asperiores nemo porro quibusdam totam inventore! \
+        Libero, consectetur?';
+}
