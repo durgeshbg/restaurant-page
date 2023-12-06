283354375d2c41a1efe569219e457604a6c2144b
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-06 18:38:46 +0530

    test contact module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 import load from './modules/load';
 import home from './modules/home';
+import contact from './modules/contact';
 
 function init() {
     load();
     home();
+    contact();
 }
 init();
