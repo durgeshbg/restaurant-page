e7a419adff4592ae32253dbb4341b0cac203a5e6
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2023-12-06 18:41:11 +0530

    test menu module

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import './style.css';
 import load from './modules/load';
 import home from './modules/home';
 import contact from './modules/contact';
+import menu from './modules/menu';
 
 function init() {
     load();
     home();
     contact();
+    menu();
 }
 init();
